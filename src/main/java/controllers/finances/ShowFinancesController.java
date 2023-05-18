826d0821209c2fd52683e9b7997b9388c559d15c
SantiagoVelasco <05santiagovelasco@gmail.com>
2023-05-17 21:41:49 -0500

    Add filter cards in showFinances module

--- src/main/java/controllers/finances/ShowFinancesController.java
+++ src/main/java/controllers/finances/ShowFinancesController.java
@@ -6,12 +6,19 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import models.Model;
+import models.interfaces.FilterCard;
 import models.interfaces.Finance;
 
 import java.net.URL;
 import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ShowFinancesController implements Initializable {
@@ -25,8 +32,6 @@ public class ShowFinancesController implements Initializable {
     @FXML
     private DatePicker dpDateTo;
     @FXML
-    private HBox hbFiltersContainer;
-    @FXML
     private TableView<Finance> tblFinances;
     @FXML
     private TableColumn<Finance, Double> colIncome;
@@ -36,6 +41,11 @@ public class ShowFinancesController implements Initializable {
     private TableColumn<Finance, String> colDescription;
     @FXML
     private TableColumn<Finance, Date> colDate;
+    @FXML
+    private HBox hbFiltersContainer;
+
+    private final List<FilterCard> filters = new ArrayList<>();
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -73,6 +83,21 @@ public class ShowFinancesController implements Initializable {
 
     @FXML
     private void selectTypeFinanceFilter() {
+        if(cbTypeFinance.getValue() == null){
+            return;
+        }
+        FilterCard currentFilterCard = null;
+        for (FilterCard filterCard: filters
+        ) {
+            if(filterCard.getType().equals("FilterType")){
+                hbFiltersContainer.getChildren().remove(filterCard.getCard());
+                currentFilterCard = filterCard;
+            }
+        }
+        if(currentFilterCard != null){
+            filters.remove(currentFilterCard);
+        }
+        addFilter("FilterType", "Filtrar por tipo", cbTypeFinance.getValue().toString());
     }
 
     @FXML
@@ -81,5 +106,82 @@ public class ShowFinancesController implements Initializable {
 
     @FXML
     private void getFinances() {
+    }
+
+    @FXML
+    private void selectDatePicker() {
+        if(dpDateTo.getValue() != null && dpDateFrom.getValue() != null){
+            FilterCard currentFilterCard = null;
+            for (FilterCard filterCard: filters
+            ) {
+                if(filterCard.getType().equals("FilterDate")){
+                    hbFiltersContainer.getChildren().remove(filterCard.getCard());
+                    currentFilterCard = filterCard;
+                }
+            }
+            if(currentFilterCard != null){
+                filters.remove(currentFilterCard);
+            }
+            addFilter("FilterDate", "Filtrar por fecha", "Desde: " +
+                    dpDateFrom.getValue().toString() + " | Hasta: " + dpDateTo.getValue().toString());
+        }
+    }
+
+    private void addFilter(String filterType, String tittle, String information){
+
+        AnchorPane filterCardOwner = new AnchorPane();
+        filterCardOwner.setPrefSize(200, 200);
+
+        Pane contentPane = new Pane();
+        contentPane.setPrefSize(200, 62);
+        contentPane.setStyle("-fx-background-color: white; -fx-padding: 30; -fx-background-radius: 5; -fx-background-insets: 10 10 10 10;");
+
+        Label filterLabelName = new Label(tittle);
+        filterLabelName.setLayoutX(14);
+        filterLabelName.setLayoutY(14);
+        contentPane.getChildren().add(filterLabelName);
+
+        Label filterLabelInformation = new Label(information);
+        filterLabelInformation.setLayoutX(14);
+        filterLabelInformation.setLayoutY(31);
+        filterLabelInformation.setStyle("-fx-text-fill: #000000b2; -fx-font-size: 10;");
+        contentPane.getChildren().add(filterLabelInformation);
+
+        Button closeButton = new Button("X");
+        closeButton.setLayoutX(166);
+        closeButton.setLayoutY(12);
+        closeButton.setStyle("-fx-background-color: transparent; -fx-cursor: hand");
+        closeButton.setTextFill(Color.RED);
+        closeButton.setFont(new Font(10));
+
+        closeButton.setOnAction(e -> removeFilter(filterType, filterCardOwner));
+
+        contentPane.getChildren().add(closeButton);
+        filterCardOwner.getChildren().add(contentPane);
+
+        hbFiltersContainer.getChildren().add(filterCardOwner);
+
+        filters.add(new FilterCard(filterType, filterCardOwner));
+    }
+
+    private void removeFilter(String filterType, AnchorPane filterCard){
+        hbFiltersContainer.getChildren().remove(filterCard);
+        FilterCard currentFilterCard = null;
+        for (FilterCard fc :
+                filters) {
+            if(fc.getCard().equals(filterCard)){
+                currentFilterCard = fc;
+            }
+        }
+        if(currentFilterCard != null){
+            filters.remove(currentFilterCard);
+        }
+        switch (filterType) {
+            case "FilterType" -> cbTypeFinance.setValue(null);
+            case "FilterDate" -> {
+                dpDateTo.setValue(null);
+                dpDateFrom.setValue(null);
+            }
+        }
     }
 }
